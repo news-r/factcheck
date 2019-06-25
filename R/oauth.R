ce2fa3faedba0e876e133ff66c039fce7a2358fd
John Coene <jcoenep@gmail.com>
2019-06-25 13:02:38 +0800

    oauth improved

--- R/oauth.R
+++ R/oauth.R
@@ -2,7 +2,7 @@
 #' 
 #' Create an OAuth token.
 #' 
-#' @param token A previously generated token.
+#' @param token A previously generated token or the path to the saved \code{RDS} file.
 #' @param id,secret Your credentials.
 #' 
 #' @return Invisibly returns an object of class \code{Token2.0}.
@@ -16,9 +16,18 @@
 #' }
 #' 
 #' @export
-fact_token <- function(token = NULL, id, secret){
+fact_token <- function(token = Sys.getenv("FACTCHECK_TOKEN"), id, secret){
 
-  if(!is.null(token)){
+  # set to NULL if does not exists
+  if(nchar(token) == 0) token <- NULL
+
+  if(!inherits(token, "Token2.0")){
+    options(FACTCHECK_TOKEN = token)
+    return(invisible(token))
+  }
+
+  if(!inherits(token, "character")){
+    token <- readRDS(token)
     options(FACTCHECK_TOKEN = token)
     return(invisible(token))
   }
